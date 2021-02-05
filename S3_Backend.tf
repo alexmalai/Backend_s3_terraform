1ca1bdad8c598edcc64e431fc4ba13bd5cc70489
alexmalai <4malex@gmail.com>
2021-02-05 18:57:10 -0500

    adding state file

--- S3_Backend.tf
+++ S3_Backend.tf
@@ -40,12 +40,12 @@ resource "aws_dynamodb_table" "terraform_locks" {
   }
 }
 
-terraform {
-  backend "s3" {
-    bucket = "terraform-state-amalai-s31q"
-    key    = "global/s3/terraform.tfstate"
-    region = "us-east-1"
-    dynamodb_table = "terraform_state_locking"
-    encrypt = true
-  }
-}
+# terraform {
+#   backend "s3" {
+#     bucket = "terraform-state-amalai-s31q"
+#     key    = "global/s3/terraform.tfstate"
+#     region = "us-east-1"
+#     dynamodb_table = "terraform_state_locking"
+#     encrypt = true
+#   }
+# }
